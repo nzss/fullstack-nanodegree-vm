e4777e14490a39e183c5f5a7871cdfb2d9e60d2a
Nilu Zhao <niluzhao@Nilus-MacBook-Air.local>
2017-02-06 21:45:40 -0500

    initial commit for sql tournament project

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -6,40 +6,62 @@
 import psycopg2
 
 
-def connect():
+def connect():    
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
+    conn = connect()
+    c = conn.cursor()
+    query = "delete from matches;"
+    c.execute(query)
+    conn.commit()
+    conn.close()
     """Remove all the match records from the database."""
 
 
 def deletePlayers():
+    conn = connect()
+    c = conn.cursor()
+    query = "delete from players;"
+    c.execute(query)
+    conn.commit()
+    conn.close()
     """Remove all the player records from the database."""
 
 
 def countPlayers():
+    conn = connect()
+    c = conn.cursor()
+    query = "select count(*) from players;"
+    c.execute(query)
+    count = c.fetchall()[0][0]
+    conn.close()
+    return count
     """Returns the number of players currently registered."""
 
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
+    conn = connect()
+    c = conn.cursor()
+    query = "insert into players (name) values ($$" + name + "$$);"
+    c.execute(query)
+    conn.commit()
+    conn.close()
+    """Adds a player to the tournament database.  
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
     Args:
       name: the player's full name (need not be unique).
     """
+    
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
-
     The first entry in the list should be the player in first place, or a player
     tied for first place if there is currently a tie.
-
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
         id: the player's unique id (assigned by the database)
@@ -47,20 +69,33 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    query = "select * from standings;"
+    c.execute(query)
+    list_standings = c.fetchall()
+    conn.close()
+    print 'STANDINGS', list_standings
+    return list_standings      
 
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
-
     Args:
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    conn = connect()
+    c = conn.cursor()
+    query = "insert into matches (player1, player2, winner) values (" + str(winner)+ ", "+ str(loser)+ ", "+ str(winner)+ ");"
+    c.execute(query)
+    conn.commit()
+    conn.close()
+
  
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
@@ -73,5 +108,17 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    conn = connect()
+    c = conn.cursor()
+    query = "select id, name from standings;"
+    c.execute(query)
+    result = c.fetchall()
+    conn.close()
+    list_pairs = []
+    for i in range(len(result)):
+        if (i%2 ==0):
+            pair = (result[i][0], result[i][1], result[i+1][0], result[i+1][1])
+            list_pairs.append(pair)
+    return list_pairs
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,49 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- create database
+create database tournament;
+
+-- connect to db:
+\c tournament;
+
+-- create tables:
+create table players (id serial, name text);
+create table matches (id serial, player1 int, player2 int, winner int);
+--drop table if exists players;
+
+-- create views:
+create view players_score as 
+	select players.id, count(winner) as score
+	from players left join matches as sub 
+	on players.id = sub.winner 
+	group by players.id
+	order by score desc;
+
+create view player1_matches as 
+	select players.id, count(player1) as count1 
+	from (players left join matches 
+	on players.id = matches.player1) 
+	group by players.id;
+
+create view player2_matches as 
+	select players.id, count(player2) as count2 
+	from (players left join matches 
+	on players.id = matches.player2) 
+	group by players.id;
+
+create view match_agg as 
+	select player1_matches.id, count1 + count2 as num_matches 
+	from (player1_matches full outer join player2_matches 
+	on player1_matches.id = player2_matches.id);
+
+create view standings as 
+	select players_name_score.id, players_name_score.name, players_name_score.score, match_agg.num_matches
+	from (select players_score.id, players.name, players_score.score 
+		from players_score left join players  
+		on players_score.id = players.id) as players_name_score
+	left join match_agg on players_name_score.id = match_agg.id
+	order by score desc;
+
+
 
